18498336161c5198c40ed44fb45cffa1c8d6d89d
hal4h <102402142+hal4h@users.noreply.github.com>
2024-01-17 21:10:46 -0600

    Update Obstacles.cpp

--- GameTest/Obstacles.cpp
+++ GameTest/Obstacles.cpp
@@ -9,17 +9,17 @@
 Obstacles::Obstacles()
 {
     initializeSprites();
-    //initializeCoordinates(sandHeight, userDepth);
+    // initializeCoordinates(sandHeight, userDepth);
     //
 }
 
 Obstacles::~Obstacles()
 {
-    for (CSimpleSprite* rockSprite : rockSprites)
+    for (CSimpleSprite *rockSprite : rockSprites)
     {
         delete rockSprite;
     }
-    for (CSimpleSprite* tireSprite : tireSprites)
+    for (CSimpleSprite *tireSprite : tireSprites)
     {
         delete tireSprite;
     }
@@ -28,11 +28,11 @@ Obstacles::~Obstacles()
 void Obstacles::Update(float deltaTime)
 {
     // Update the obstacles
-    for (CSimpleSprite* rockSprite : rockSprites)
+    for (CSimpleSprite *rockSprite : rockSprites)
     {
         rockSprite->Update(deltaTime);
     }
-    for (CSimpleSprite* tireSprite : tireSprites)
+    for (CSimpleSprite *tireSprite : tireSprites)
     {
         tireSprite->Update(deltaTime);
     }
@@ -46,207 +46,261 @@ void Obstacles::Update(float deltaTime)
     // }
 }
 
-void Obstacles::DrawObstacles(float sandHeight, float yPos)
-
+void Obstacles::DrawObstacles(float yPos)
 {
-    initializeCoordinates(sandHeight, yPos);
+    float currentX, currentY;
 
     // Draw the obstacles
-    for (CSimpleSprite* rockSprite : rockSprites)
+    for (CSimpleSprite *rockSprite : rockSprites)
     {
+        rockSprite->GetPosition(currentX, currentY);
+        rockSprite->SetPosition(currentX, currentY + yPos); // Add yPos to the current Y position, Set the updated position
+
         rockSprite->Draw();
     }
-    // for (CSimpleSprite *tireSprite : tireSprites)
-    // {
-    //     tireSprite->Draw();
-    // }
-    // for (CSimpleSprite *branchSprite : branchSprites)
-    // {
-    //     branchSprite->Draw();
-    // }
-    // for (CSimpleSprite *cattailSprite : cattailSprites)
-    // {
-    //     cattailSprite->Draw();
-    // }
+    for (CSimpleSprite *tireSprite : tireSprites)
+    {
+        tireSprite->Draw();
+    }
+    for (CSimpleSprite *branchSprite : branchSprites)
+    {
+        branchSprite->Draw();
+    }
+    for (CSimpleSprite *cattailSprite : cattailSprites)
+    {
+        cattailSprite->Draw();
+    }
+    for (CSimpleSprite *crabSprite : crabSprites){
+        crabSprite->Draw();
+    }
+
+    for (CSimpleSprite *frogSprite : frogSprites)
+    {
+        frogSprite->Draw();
+    }
+
+    for (CSimpleSprite *pinkJellySprite : pinkJellySprites)
+    {
+        pinkJellySprite->Draw();
+    }
+    
+    for (CSimpleSprite *squidSprite : squidSprites)
+    {
+        squidSprite->Draw();
+    }
 }
+
 void Obstacles::initializeSprites()
 {
     // Initialize rock sprites
     for (int i = 0; i < ROCK_COUNT; ++i)
     {
-        CSimpleSprite* rockSprite = initRock();
+        CSimpleSprite *rockSprite = initRock();
         rockSprites.push_back(rockSprite);
     }
 
+    //Initialize tire sprites
+     for (int i = 0; i < TIRE_COUNT; ++i)
+     {
+      CSimpleSprite* tireSprite = initTire();
+      tireSprites.push_back(tireSprite);
+     }
+
+     for (int i = 0; i < FISH_COUNT; i++)
+     {
+        CSimpleSprite* fishSprite = initFish();
+        fishSprites.push_back(fishSprite);
+     }
+     
+
+    // Initialize branch sprites
+    for (int i = 0; i < BRANCH_COUNT; ++i)
+    {
+        CSimpleSprite *branchSprite = initBranch();
+        branchSprites.push_back(branchSprite);
+    }
+
+    // Initialize cattail sprites
+    for (int i = 0; i < CATTAIL_COUNT; ++i)
+    {
+        CSimpleSprite *cattailSprite = initCattail();
+        cattailSprites.push_back(cattailSprite);
+    }
+
+    // Initialize crab sprites
+    for (int i = 0; i < CRAB_COUNT; ++i)
+    {
+        CSimpleSprite *crabSprite = initCrab();
+        crabSprites.push_back(crabSprite);
+    }
+
+    // Initialize frog sprites
+    for (int i = 0; i < FROG_COUNT; ++i)
+    {
+        CSimpleSprite *frogSprite = initFrog();
+        frogSprites.push_back(frogSprite);
+    }
+
+    // Initialize pink jelly sprites
+    for (int i = 0; i < PINK_JELLY_COUNT; ++i)
+    {
+        CSimpleSprite *pinkJellySprite = initPinkJelly();
+        pinkJellySprites.push_back(pinkJellySprite);
+    }
+
+    // Initialize squid sprites
+    for (int i = 0; i < SQUID_COUNT; ++i)
+    {
+        CSimpleSprite *squidSprite = initSquid();
+        squidSprites.push_back(squidSprite);
+    }
+
     // Initialize tire sprites
-  //  for (int i = 0; i < TIRE_COUNT; ++i)
-  //  {
-     //   CSimpleSprite* tireSprite = nullptr;
-      //  initTire(tireSprite);
-     //   tireSprites.push_back(tireSprite);
-  //  }
+    for (int i = 0; i < YELLOW_JELLY_COUNT; ++i)
+    {
+        CSimpleSprite *yellowJellySprites = initYellowJelly();
+        yellowJellySprites.push_back(tireSprite);
+    }
 
-    // // Initialize branch sprites
-    // for (int i = 0; i < BRANCH_COUNT; ++i)
-    // {
-    //     CSimpleSprite *branchSprite = nullptr;
-    //     initBranch(branchSprite);
-    //     branchSprites.push_back(branchSprite);
-    // }
+    // Initialize turtle sprites
+    for (int i = 0; i < TURTLE_COUNT; ++i)
+    {
+        CSimpleSprite *turtleSprite = initTurtle();
+        turtleSprites.push_back(turtleSprite);
+    }
 
-    // // Initialize cattail sprites
-    // for (int i = 0; i < CATTAIL_COUNT; ++i)
-    // {
-    //     CSimpleSprite *cattailSprite = nullptr;
-    //     initCattail(cattailSprite);
-    //     cattailSprites.push_back(cattailSprite);
-    // }
+    // Initialize watermelon sprites
+    for (int i = 0; i < WATERMELON_COUNT; ++i)
+    {
+        CSimpleSprite *watermelonSprite = initWatermelon();
+        watermelonSprites.push_back(watermelonSprite);
+    }
 
-    // // Initialize crab sprites
-    // for (int i = 0; i < CRAB_COUNT; ++i)
-    // {
-    //     CSimpleSprite *crabSprite = nullptr;
-    //     initCrab(crabSprite);
-    //     crabSprites.push_back(crabSprite);
-    // }
+    // Initialize yellow jelly sprites
+    for (int i = 0; i < YELLOW_JELLY_COUNT; ++i)
+    {
+        CSimpleSprite *yellowJellySprite = initYellowJelly();
+        yellowJellySprites.push_back(yellowJellySprite);
+    }
 
-    // // Initialize frog sprites
-    // for (int i = 0; i < FROG_COUNT; ++i)
-    // {
-    //     CSimpleSprite *frogSprite = nullptr;
-    //     initFrog(frogSprite);
-    //     frogSprites.push_back(frogSprite);
-    // }
-
-    // // Initialize pink jelly sprites
-    // for (int i = 0; i < PINK_JELLY_COUNT; ++i)
-    // {
-    //     CSimpleSprite *pinkJellySprite = nullptr;
-    //     initPinkJelly(pinkJellySprite);
-    //     pinkJellySprites.push_back(pinkJellySprite);
-    // }
-
-    // // Initialize squid sprites
-    // for (int i = 0; i < SQUID_COUNT; ++i)
-    // {
-    //     CSimpleSprite *squidSprite = nullptr;
-    //     initSquid(squidSprite);
-    //     squidSprites.push_back(squidSprite);
-    // }
-
-    // // Initialize tire sprites
-    // for (int i = 0; i < TIRE_COUNT; ++i)
-    // {
-    //     CSimpleSprite *tireSprite = nullptr;
-    //     initTire(tireSprite);
-    //     tireSprites.push_back(tireSprite);
-    // }
-
-    // // Initialize turtle sprites
-    // for (int i = 0; i < TURTLE_COUNT; ++i)
-    // {
-    //     CSimpleSprite *turtleSprite = nullptr;
-    //     initTurtle(turtleSprite);
-    //     turtleSprites.push_back(turtleSprite);
-    // }
-
-    // // Initialize watermelon sprites
-    // for (int i = 0; i < WATERMELON_COUNT; ++i)
-    // {
-    //     CSimpleSprite *watermelonSprite = nullptr;
-    //     initWatermelon(watermelonSprite);
-    //     watermelonSprites.push_back(watermelonSprite);
-    // }
+    // initialize coordinates at the end
+    // initializeCoordinates(sandHeight, userDepth);
 }
 
-void Obstacles::initializeCoordinates(float sandHeight, float userDepth)
-
+void Obstacles::initializeCoordinates(float userDepth)
 {
+    const int difference = 850;
+    int start = 500;
+
+    // Calculate the vertical spacing between rock sprites
+    const float verticalSpacing = (MAX - start) / (ROCK_COUNT - 1);
+
     // Initialize coordinates for rock sprites
-   for (CSimpleSprite* rockSprite : rockSprites)
+    for (CSimpleSprite *rockSprite : rockSprites)
     {
         float randomX = generateRandomX(rockSprite);
+
         // Set Y value and other properties as needed
-        rockSprite->SetPosition(randomX, 50);
-   }
+        rockSprite->SetPosition(randomX, start);
+        start += verticalSpacing;
+    }
 
     // Initialize coordinates for tire sprites
-   // for (CSimpleSprite* tireSprite : tireSprites)
-   // {
-      //  int randomX = generateRandomX(tireSprite);
-       // Set Y value and other properties as needed
-     //   tireSprite->SetPosition(randomX, 40);
-   // }
+    // for (CSimpleSprite* tireSprite : tireSprites)
+    // {
+    //  int randomX = generateRandomX(tireSprite);
+    // Set Y value and other properties as needed
+    //   tireSprite->SetPosition(randomX, 40);
+    // }
+    //   start+=differnece;
 }
 
-CSimpleSprite* Obstacles::initRock()
+CSimpleSprite *Obstacles::initRock()
 {
-    CSimpleSprite* sprite = new CSimpleSprite(".\\TestData\\rocks.png", 2, 3);
-    sprite->CreateAnimation(0, 1.0f / 6.0f, { 0, 1, 2, 3, 4, 5 });
+    CSimpleSprite *sprite = new CSimpleSprite(".\\TestData\\rocks.png", 2, 3);
+    sprite->CreateAnimation(0, 1.0f / 6.0f, {0, 1, 2, 3, 4, 5});
     sprite->SetAnimation(0);
-    sprite->SetPosition(50, 50);
+    return sprite;
+}
+
+CSimpleSprite *Obstacles::initTire()
+{
+    CSimpleSprite *sprite = new CSimpleSprite(".\\TestData\\obstacles\\tire.png", 2, 1);
+    sprite->CreateAnimation(0, 1.0f / 6.0f, {0, 1});
+    sprite->SetAnimation(0);
+    return sprite;
+}
+CSimpleSprite *Obstacles::initFish()
+{
+    CSimpleSprite *sprite = new CSimpleSprite(".\\TestData\\obstacles\\fish.png", 4, 1);
+    sprite->CreateAnimation(0, 1.0f / 6.0f, {0, 1, 2, 3});
+    sprite->SetAnimation(0);
 
     return sprite;
 }
 
-
-void Obstacles::initTire(CSimpleSprite* sprite)
+CSimpleSprite *Obstacles::initYellowJelly()
 {
-    sprite = new CSimpleSprite(".\\TestData\\obstacles\\tire.png", 2, 1);
-    sprite->CreateAnimation(0, 1.0f / 6.0f, { 0, 1 });
+    CSimpleSprite *sprite = new CSimpleSprite(".\\TestData\\obstacles\\yellow-jelly.png", 4, 1);
+    sprite->CreateAnimation(0, 1.0f / 6.0f, {0, 1, 2, 3});
     sprite->SetAnimation(0);
 
-}
-void Obstacles::initYellowJelly(CSimpleSprite* sprite)
-{
-    sprite = new CSimpleSprite(".\\TestData\\obstacles\\yellow-jelly.png", 4, 1);
-    sprite->CreateAnimation(0, 1.0f / 6.0f, { 0, 1, 2, 3 });
-}
-void Obstacles::initFrog(CSimpleSprite* sprite)
-{
-    sprite = new CSimpleSprite(".\\TestData\\obstacles\\frog.png", 4, 4);
-    sprite->CreateAnimation(0, 1.0f / 6.0f, { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 });
+    return sprite;
 }
 
-void Obstacles::initPinkJelly(CSimpleSprite* sprite)
+CSimpleSprite *Obstacles::initFrog()
 {
-    sprite = new CSimpleSprite(".\\TestData\\obstacles\\pink-jelly.png", 4, 1);
-    sprite->CreateAnimation(0, 1.0f / 6.0f, { 0, 1, 2, 3 });
+    CSimpleSprite *sprite = new CSimpleSprite(".\\TestData\\obstacles\\frog.png", 4, 4);
+    sprite->CreateAnimation(0, 1.0f / 6.0f, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
+    sprite->SetAnimation(0);
+
+    return sprite;
 }
 
-void Obstacles::initSquid(CSimpleSprite* sprite)
+CSimpleSprite *Obstacles::initPinkJelly()
 {
-    sprite = new CSimpleSprite(".\\TestData\\obstacles\\squid.png", 4, 1);
-    sprite->CreateAnimation(0, 1.0f / 6.0f, { 0, 1, 2, 3 });
+    CSimpleSprite *sprite = new CSimpleSprite(".\\TestData\\obstacles\\pink-jelly.png", 2, 1);
+    sprite->CreateAnimation(0, 1.0f / 6.0f, {0, 1});
+    return sprite;
 }
 
-void Obstacles::initTurtle(CSimpleSprite* sprite)
+CSimpleSprite *Obstacles::initSquid()
 {
-    sprite = new CSimpleSprite(".\\TestData\\obstacles\\turtle.png", 4, 1);
-    sprite->CreateAnimation(0, 1.0f / 6.0f, { 0, 1, 2, 3 });
+    CSimpleSprite *sprite = new CSimpleSprite(".\\TestData\\obstacles\\squid.png", 5, 1);
+    sprite->CreateAnimation(0, 1.0f / 6.0f, {0, 1, 2, 3,4});
+    return sprite;
 }
 
-void Obstacles::initWatermelon(CSimpleSprite* sprite)
+CSimpleSprite *Obstacles::initTurtle()
 {
-    sprite = new CSimpleSprite(".\\TestData\\obstacles\\watermelon.png", 1, 1);
+    CSimpleSprite *sprite = new CSimpleSprite(".\\TestData\\obstacles\\turtle.png", 4, 1);
+    sprite->CreateAnimation(0, 1.0f / 6.0f, {0, 1, 2, 3});
+    return sprite;
 }
 
-void Obstacles::initCattail(CSimpleSprite* sprite)
+CSimpleSprite *Obstacles::initWatermelon()
 {
-    sprite = new CSimpleSprite(".\\TestData\\obstacles\\cattail.png", 1, 1);
+    return new CSimpleSprite(".\\TestData\\obstacles\\watermelon.png", 1, 1);
 }
 
-void Obstacles::initCrab(CSimpleSprite* sprite)
+CSimpleSprite *Obstacles::initCattail()
 {
-    sprite = new CSimpleSprite(".\\TestData\\obstacles\\crab.png", 4, 1);
-    sprite->CreateAnimation(0, 1.0f / 6.0f, { 0, 1, 2, 3 });
-}
-void Obstacles::initBranch(CSimpleSprite* sprite)
-{
-    sprite = new CSimpleSprite(".\\TestData\\obstacles\\branch.png", 1, 1);
+    return new CSimpleSprite(".\\TestData\\obstacles\\cattail.png", 1, 1);
 }
 
-float Obstacles::generateRandomX(CSimpleSprite* sprite)
+CSimpleSprite *Obstacles::initCrab()
+{
+    CSimpleSprite *sprite = new CSimpleSprite(".\\TestData\\obstacles\\crab.png", 6, 1);
+    sprite->CreateAnimation(0, 1.0f / 6.0f, {0, 1, 2, 3,4,5});
+    return sprite;
+}
+
+CSimpleSprite *Obstacles::initBranch()
+{
+    return new CSimpleSprite(".\\TestData\\obstacles\\branch.png", 1, 1);
+}
+
+float Obstacles::generateRandomX(CSimpleSprite *sprite)
 {
     // Assuming sprite is centered on its X-axis, you may need to adjust accordingly
     float spriteWidth = sprite->GetWidth();
