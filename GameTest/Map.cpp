44b6ceed885569bea19c8d38314f64cac75d7a73
hal4h <102402142+hal4h@users.noreply.github.com>
2024-01-21 16:58:07 -0600

    Update Map.cpp

--- GameTest/Map.cpp
+++ GameTest/Map.cpp
@@ -1,29 +1,22 @@
 #include "stdafx.h"
 #include "app\app.h"
 
-
 // Map.cpp
 #include "Map.h"
 #include <fstream>
 #include <sstream>
-#include <iostream>  // Include this for error handling
+#include <iostream> // Include this for error handling
 
-
-
-#define min(a,b) ((a) < (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
-
-Map::Map(int windowWidth, int windowHeight) {
+Map::Map(int windowWidth, int windowHeight)
+{
 
     mapYPosition = 2.0f; // Initial Y position of the map
 
     // Initialize the water sprite with an 8x2 animated sprite representing waves
     waterSprite = new CSimpleSprite(".\\TestData\\Ocean_SpriteSheet.png", 8, 2); // Adjust columns and rows
-    // waterSprite = new CSimpleSprite(".\\TestData\\waterfr3.png",3, 1); // Adjust columns and rows
-    // waterSprite->SetScale(2.0f);
-  // waterSprite->SetScale( windowWidth/ windowHeight);
-   //waterSprite->SetColor(0.9, 0.9, 0.9); // Light blue color
 
     sandlayer = new CSimpleSprite(".\\TestData\\newsand.png", 1, 1); // Adjust columns and rows
     sandlayer->SetPosition(sandlayer->GetWidth() / 2.0f, sandlayer->GetHeight() / 2.0f + mapYPosition);
@@ -32,91 +25,68 @@ Map::Map(int windowWidth, int windowHeight) {
     wateranim = new CSimpleSprite(".\\TestData\\waveanim.png", 17, 1); // Adjust columns and rows
     wateranim->SetAngle(3.14f);
 
-    //wateranim->SetPosition(wateranim->GetWidth() / 2.0f, sandlayer->GetHeight() + wateranim->GetHeight() / 2.0f + mapYPosition);
-
-     //sandlayer->Draw();
-       // Calculate scale to cover the entire screen
-      // float scale = windowHeight / waterSprite->GetHeight();
-       //waterSprite->SetScale(scale);
-      // tileset = new CSimpleSprite(".\\TestData\\background.png", 1, 1); // Adjust columns and rows
-    //   rows = 1;
-     //  cols = 1;
     rows = 322;
     cols = 24;
-    //  tileset = new CSimpleSprite(".\\TestData\\GameBackground-Tileset.png", 17, 13); // Adjust columns and rows
-     //  rows = 13;
-     //  cols = 17;
-     // float tileWidth = 32.0f; // Adjust as needed
-     // float tileHeight = 96.0f; // Adjust as needed
 
-    //  float scaleX = tileWidth / tileset->GetWidth();
-    //  float scaleY = tileHeight / tileset->GetHeight();
-    //  tileset->SetScale(scaleX/ scaleY);
-
-      // Initialize the tilemap
-      // initialize it as a 2D matrix with -all tiles set to WATER
-
-      //TODO: change values to work with screen dimentions
-      //int numRows = 40; // Adjust as needed
-     // int numColumns = 160; // Adjust as needed
-     // tilemap.resize(numRows, std::vector<TileType>(numColumns, WATER));
-
-
-
-
-      // Create animations for the water spr
+    // Create animations for the water spr
     float animationSpeed = 1.0f / 6.0f; // Adjust as needed
-    waterSprite->CreateAnimation(ANIMATION_ID_WATER, animationSpeed, { 0, 1,2 });
+    waterSprite->CreateAnimation(ANIMATION_ID_WATER, animationSpeed, {0, 1, 2});
 
     float waterSpeed = 1.0f / 6.0f; // Adjust as needed
-    wateranim->CreateAnimation(ANIMATION_ID_WATER, animationSpeed, { 0, 1,2 ,3,4,5,6,7,8,9,10,11,12,12,13,14,15,16 });
+    wateranim->CreateAnimation(ANIMATION_ID_WATER, animationSpeed, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 13, 14, 15, 16});
     scrollSpeed = 1.0f / 9.0f; // Adjust as needed
-    //  LoadTilemapFromFile("Layer0.txt");  // Replace with your actual file name
 
-    obstacles.initializeSprites();
+    // obstacles.initializeSprites();
     obstacles.initializeCoordinates(mapYPosition);
 
+    // init chest
+    chest = new CSimpleSprite(".\\TestData\\chest.png", 1, 1);
 }
 
-Map::~Map() {
+Map::~Map()
+{
     delete waterSprite;
 }
 
-void Map::Update(float deltaTime, float depth) {
-   // delta = deltaTime;
+void Map::Update(float deltaTime)
+{
+    // delta = deltaTime;
     // Update any animation or logic based on deltaTime
     waterSprite->Update(deltaTime);
     wateranim->Update(deltaTime);
     // waterSprite->SetColor(0.9 - depth /5, 0.9 -depth/5, 0.9 - depth / 5); // Light blue color
     obstacles.Update(deltaTime);
 
-   //if (App::GetController().GetLeftThumbStickY() > 0.5f) {
-       ScrollDown(deltaTime);
+    // if (App::GetController().GetLeftThumbStickY() > 0.5f) {
+    ScrollDown(deltaTime);
 
-   // }
-    //else if (App::GetController().GetLeftThumbStickY() < -0.5f) {
-     //   ScrollUp(deltaTime);
-   // }
-    //depth = min(depth, 5.0f);
+    // }
+    // else if (App::GetController().GetLeftThumbStickY() < -0.5f) {
+    //   ScrollUp(deltaTime);
+    // }
+    // depth = min(depth, 5.0f);
     // Calculate new RGB values based on depth
-  //  float red = max(0.0f, 0.9f - depth / 5.0f);
- //  float green = max(0.0f, 0.9f - depth / 5.0f);
-   // float blue = max(0.0f, 0.9f - depth / 5.0f);
+    //  float red = max(0.0f, 0.9f - depth / 5.0f);
+    //  float green = max(0.0f, 0.9f - depth / 5.0f);
+    // float blue = max(0.0f, 0.9f - depth / 5.0f);
 
     // Set the new color for the water sprite
-  //  waterSprite->SetColor(red, green, blue);
-   // waterSprite->SetColor(0.678f, 0.8474f, 0.902f);
+    //  waterSprite->SetColor(red, green, blue);
+    // waterSprite->SetColor(0.678f, 0.8474f, 0.902f);
 
-  //  sandlayer->Update(deltaTime);
+    //  sandlayer->Update(deltaTime);
 }
 
-void Map::Draw() {
+void Map::Draw()
+{
     // Draw the tilemap
 
     float scaleX = static_cast<float>(APP_VIRTUAL_WIDTH) / (cols * waterSprite->GetWidth());
 
-    for (int row = 0; row < rows; ++row) {
-        for (int col = 0; col < cols; ++col) {
+    for (int row = 0; row < rows; ++row)
+    {
+        for (int col = 0; col < cols; ++col)
+        {
 
             // Calculate the position for each tile based on its row and column
             float xPos = col * waterSprite->GetWidth();
@@ -134,38 +104,46 @@ void Map::Draw() {
     sandlayer->SetPosition(sandlayer->GetWidth() / 2.0f, sandlayer->GetHeight() / 2.0f + mapYPosition);
     sandlayer->Draw();
 
-
     float start = wateranim->GetWidth() / 2.0f;
     float waterAnimWidth = wateranim->GetWidth();
 
-    for (int i = 0; i < cols; i++) {
+    for (int i = 0; i < cols; i++)
+    {
         float xPos = start + i * waterAnimWidth;
         wateranim->SetAnimation(ANIMATION_ID_WATER);
         wateranim->SetPosition(xPos, sandlayer->GetHeight() + wateranim->GetHeight() / 2.0f + mapYPosition);
         wateranim->Draw();
     }
     obstacles.DrawObstacles(2);
+
+    // Draw the chest
+    chest->SetPosition(chest->GetWidth() / 2.0f, chest->GetHeight() / 2.0f + mapYPosition);
+    chest->Draw();
+
+    void drawChest()
+
 }
 
-void Map::ScrollUp(float deltatime) {
+void Map::ScrollUp(float deltatime)
+{
     mapYPosition += scrollSpeed * deltatime;
 
     mapYPosition = min(mapYPosition, 0.0f);
-
 }
 
-void Map::ScrollDown(float deltatime) {
+void Map::ScrollDown(float deltatime)
+{
     mapYPosition -= scrollSpeed * deltatime;
 
+    // ensure it doesnt go past top of screen
     mapYPosition = max(mapYPosition, -static_cast<float>(rows * waterSprite->GetHeight()));
-
 }
 
-
-
-void Map::LoadTilemapFromFile(const char* filename) {
+void Map::LoadTilemapFromFile(const char *filename)
+{
     std::ifstream file(filename);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         // Handle error: unable to open file
         return;
     }
@@ -173,12 +151,14 @@ void Map::LoadTilemapFromFile(const char* filename) {
     tilemap.clear(); // Clear existing tilemap data
 
     std::string line;
-    while (std::getline(file, line)) {
+    while (std::getline(file, line))
+    {
         std::istringstream iss(line);
         std::vector<TileType> row;
 
         int tileType;
-        while (iss >> tileType) {
+        while (iss >> tileType)
+        {
             row.push_back(static_cast<TileType>(tileType));
         }
 
@@ -188,9 +168,27 @@ void Map::LoadTilemapFromFile(const char* filename) {
     file.close();
 }
 
-
-
 // method to return mapY location
-float Map::GetYPos() {
+float Map::GetYPos()
+{
     return mapYPosition;
+}
+
+// returns vectors of all visibile objects from calling objects method
+std::vector<Obstacle> Map::getVisibleObstacles()
+{
+    return obstacles.getVisibleObstacles();
+}
+
+// display meters left to finish
+
+void Map::drawChest()
+{
+    chest->SetPosition(chest->GetWidth() / 2.0f, chest->GetHeight() / 2.0f + mapYPosition);
+    chest->Draw();
+}
+
+CSimpleSprite* Map::getChest()
+{
+    return chest;   
 }
